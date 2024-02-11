61c8229a3cceb1f8fec6e2bc727aa35837cf570c
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-12 05:25:42 +0530

    all products done(static)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-
 function App() {
   return (
     <div className="app">
--- src/Product.jsx
+++ src/Product.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./Product.css";
 
 function Product() {
-  return <div className="product">Product</div>;
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>16.01</strong>
+        </p>
+        <div className="product__rating">
+          <p>*</p>
+        </div>
+      </div>
+      <img
+        src="https://rukminim2.flixcart.com/image/416/416/koudfgw0/regionalbooks/r/x/m/the-lean-startup-original-imag37egg7hyqpmx.jpeg?q=70&crop=false"
+        alt=""
+      />
+      <button>Add to Basket</button>
+    </div>
+  );
 }
 
 export default Product;

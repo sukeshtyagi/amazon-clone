d645ba9c0d29fffd10d9da3c0b895991e3bd1fd0
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-11 22:53:35 +0530

    Home banner created

---
+++ src/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://s.hdnux.com/photos/01/22/16/43/21555441/4/rawImage.jpg"
+          alt=""
+          className="home__image"
+        />
+        <div className="home__row">
+          {/*product*/}
+          {/*product*/}
+        </div>
+        <div className="home__row">
+          {/*product*/}
+          {/*product*/}
+          {/*product*/}
+        </div>
+        <div className="home__row">{/*product*/}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

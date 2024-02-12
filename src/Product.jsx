3f9f7ebdf4c2ead6c406a08a96499d337bd4cd23
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-12 06:04:51 +0530

    products code updated

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>16.01</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>*</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p key={i}>*</p>
+            ))}
         </div>
       </div>
       <img
-        src="https://rukminim2.flixcart.com/image/416/416/koudfgw0/regionalbooks/r/x/m/the-lean-startup-original-imag37egg7hyqpmx.jpeg?q=70&crop=false"
+        src={image}
         alt=""
       />
       <button>Add to Basket</button>

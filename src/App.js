b73093a8c309c69200401189841b90d1b7980e41
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-12 07:41:45 +0530

    Routing intialised

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <h1>checkout comp</h1>
+              </>
+            }
+          />
+          <Route
+            path="/*"
+            element={<Navigate to='/'/>}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
